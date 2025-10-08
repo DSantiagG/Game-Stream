b591622c1ae2f2108189f767fa9a9530e69aea51
David Santiago Giron Muñoz <98716905+DSantiagG@users.noreply.github.com>
2025-10-08 10:52:54 -0500

    Initial Commit

---
+++ Game Stream/Game_StreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_StreamApp.swift
+//  Game Stream
+//
+//  Created by David Giron on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Game_StreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
